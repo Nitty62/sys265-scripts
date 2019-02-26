d242c98ed8be458a7ac53365c7849ef967788da6
Nitty62 <nick.sampson@mymail.champlain.edu>
2019-02-26 14:22:15 -0500

    added publick key

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -5,3 +5,10 @@
 #Removes roots ability to ssh in
 
 echo "testing 123"
+sudo useradd sys265
+sudo mkdir /home/sys265/.ssh
+sudo cp linux/public-keys/sys265.pub /home/sys265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh
+sudo chmod 600 /home/sys265/.ssh/authorized_keys
+sudo chown -R sys265:sys265 /home/sys265/.ssh
+
